b61036d89da279cc7e0c0e1ca8cd2909281ada67
Ivan Martin <ivmg19@gmail.com>
2015-07-06 22:11:56 +0200

    Innitial commit
    Setup of the project

---
+++ pcmratingbot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import lxml.html
+import re
+
+def wordcount(text):
+    # Find all non-whitespace patterns.
+    list = re.findall("(\S+)", text)
+    # Return length of resulting list.
+    return len(list)
+	
+text = raw_input("¿Que juego quieres buscar?")
+count = wordcount(text)
+if count == 0:
+    base_url = "http://www.pcmrating.com/games?utf8=%E2%9C%93&q%5Btitle_cont%5D=" + text + "&commit=Search"
+elif count >= 1:
+    textx = text.replace(":", "%3A")  #If there is any : it'll be changed
+    textt = textx.replace(" ", "+") #Replace spaces
+    base_url = "http://www.pcmrating.com/games?utf8=%E2%9C%93&q%5Btitle_cont%5D=" + textt + "&commit=Search"
+content = lxml.html.parse(base_url)
+links = content.xpath('//div[@class="row game even"]/div[@class="col-xs-2 game-name"]/a/@href')
+print links
