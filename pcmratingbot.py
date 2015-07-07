6169aa3b48b987fa5fce23aeb84c403d1d1c13ac
Ivan Martin <ivmg19@gmail.com>
2015-07-07 17:57:58 +0200

    get the game info

--- pcmratingbot.py
+++ pcmratingbot.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import lxml.html
 import re
+import urllib
 
 def wordcount(text):
     # Find all non-whitespace patterns.
@@ -20,5 +20,11 @@ elif count >= 1:
     base_url = 'http://www.pcmrating.com/games?utf8=%E2%9C%93&q%5Btitle_cont%5D=' + textt + '&commit=Search'
 content = lxml.html.parse(base_url)
 links = content.xpath('//div[@class="row game even"][1]/div[@class="col-xs-2 game-name"][1]/a/@href[1]')[1] #Get the first href query
-nurl = 'http://www.pcmrating.com/' + links
-print nurl
+nurl = 'http://www.pcmrating.com' + links
+scores = lxml.html.parse(nurl)
+score_titles = scores.xpath('//div[@class="rating row bottom-separator"]/div[@class="col-xs-12"]/ul/li/label/text()')
+for elem in score_titles:
+	print elem
+score_results = scores.xpath('//div[@class="rating row bottom-separator"]/div[@class="col-xs-12"]/ul/li/text()')
+for elem2 in score_results:
+	print elem2
